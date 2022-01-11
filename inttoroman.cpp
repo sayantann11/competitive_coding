bd04be6586b758b6ffe6def163c6f927d2d293cf
sayantann11 <64836894+sayantann11@users.noreply.github.com>
2022-01-11 16:14:12 +0530

    Create inttoroman.cpp

---
+++ inttoroman.cpp
@@ -0,0 +1,17 @@
+int arabic[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+		string roman[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+		int i=0;
+
+		string result;
+
+		while(num>0){
+			if (num>=arabic[i]){
+				result=result+roman[i];
+				num-=arabic[i];
+			}
+			else{
+				++i;
+			}
+		}
+		return result;
